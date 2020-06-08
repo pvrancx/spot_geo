aae88d538f5450160a82dbbf1782c4ec582f3cda
Peter Vrancx <peter@prwoler.io>
2020-06-08 19:26:41 +0100

    replace layer

--- geo/models.py
+++ geo/models.py
@@ -5,5 +5,5 @@ import torch.nn as nn
 def create_fcn_resnet(**kwargs) -> nn.Module:
     """Create fully convolutional resnet for 1 channel image segmentation"""
     m = fcn_resnet50(pretrained=False, num_classes=1, **kwargs)
-    m.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+    m.backbone._modules['conv1'] = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
     return m
